21da74848c4fbdc46f9f103633c57872963c2b94
lhumphries1 <65079306+lhumphries1@users.noreply.github.com>
2020-05-12 19:26:44 -0400

    Iteration 1 is complete

--- src/main/java/GuessingGameApp.java
+++ src/main/java/GuessingGameApp.java
@@ -1,7 +1,17 @@
+import java.util.Scanner;
+
 public class GuessingGameApp {
 
     public static void main(String[] args) {
+        System.out.println("Welcome to the Guessing Game!");
+        System.out.println("Please enter a number between 1-10");
+        Scanner input = new Scanner(System.in);
+        int guess = input.nextInt();
+        if (guess == 7) {
+            System.out.println("Congratulations you have won!");
+        } else {
+            System.out.println("You have lost!");
+        }
 
     }
-
 }
