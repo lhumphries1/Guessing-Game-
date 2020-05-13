e60c265732a56ee542e47b6867a80ad4df9b60a6
lhumphries1 <65079306+lhumphries1@users.noreply.github.com>
2020-05-13 01:33:36 -0400

    fortuneTellerApp

--- src/main/java/GuessingGameApp.java
+++ src/main/java/GuessingGameApp.java
@@ -4,13 +4,17 @@ public class GuessingGameApp {
 
     public static void main(String[] args) {
         System.out.println("Welcome to the Guessing Game!");
-        System.out.println("Please enter a number between 1-10");
+        System.out.println("Select your WINNING number between 1-10");
         Scanner input = new Scanner(System.in);
         int guess = input.nextInt();
+
         if (guess == 7) {
             System.out.println("Congratulations you have won!");
         } else {
             System.out.println("You have lost!");
+        } if (guess == 0){
+            System.out.println("Select your WINNING number between 1-10");
+          guess = input.nextInt();
         }
 
     }
